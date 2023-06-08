0cd6b22b7a20e2bb2a6db6c187d0efff9ff5559b
mverzi <maggie.verzi@gmail.com>
2023-06-08 11:44:57 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-document.querySelector('button').addEventListener('click', apiRequest)
-
-async function apiRequest(){
-    const ponyName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://busy-jade-hatchling-veil.cyclic.app/api/${ponyName}`)
-        const data = await response.json()
-        console.log(data)
-        document.querySelector('h2').innerText = data.name
-    } catch(error) {
-        console.log(error)
-    }
-}
+alert('working')
